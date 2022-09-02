029dc35cf4462dc3369c3fcc3e67b9a89eea9118
Patrik Freij <patrik.t.freij@gmail.com>
2022-09-02 21:44:59 +0200

    Add ability to create a character sheet
    This generates a light weight character sheet with the stats available
    at the moment.

--- src/Command/CharacterSheet.php
+++ src/Command/CharacterSheet.php
@@ -3,8 +3,8 @@
 namespace loyen\DndbCharacterSheet\Command;
 
 use Composer\Script\Event;
-use loyen\DndbCharacterSheet\Exception\CharacterFileReadException;
 use loyen\DndbCharacterSheet\Importer;
+use loyen\DndbCharacterSheet\Sheet;
 
 class CharacterSheet
 {
@@ -15,15 +15,23 @@ class CharacterSheet
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
         require_once $vendorDir . '/autoload.php';
 
-        $characterId = $event->getArguments()[0] ?? throw new \Exception('No character ID inputted.');
+        $arguments = $event->getArguments();
+        $characterId = \array_pop($arguments);
+
+        $characterId = $characterId ?? throw new \Exception('No character ID inputted.');
         $characterId = intval($characterId);
 
         $character = Importer::importFromApiById($characterId);
 
-        echo \json_encode(
-            $character,
-            \JSON_PRETTY_PRINT
-        );
+        if (\in_array('--json', $arguments, true)) {
+            echo \json_encode(
+                $character,
+                \JSON_PRETTY_PRINT
+            );
+        } else {
+            $sheet = new Sheet();
+            echo $sheet->render($character);
+        }
 
         echo \PHP_EOL;
         exit($exitCode);
@@ -36,13 +44,21 @@ class CharacterSheet
         $vendorDir = $event->getComposer()->getConfig()->get('vendor-dir');
         require_once $vendorDir . '/autoload.php';
 
-        $characterFilePath = $event->getArguments()[0] ?? throw new \Exception('No file inputted.');
+        $arguments = $event->getArguments();
+        $filePath = \array_pop($arguments);
+
+        $characterFilePath = $filePath ?? throw new \Exception('No file inputted.');
         $character = Importer::importFromFile($characterFilePath);
 
-        echo \json_encode(
-            $character,
-            \JSON_PRETTY_PRINT
-        );
+        if (\in_array('--json', $arguments, true)) {
+            echo \json_encode(
+                $character,
+                \JSON_PRETTY_PRINT
+            );
+        } else {
+            $sheet = new Sheet();
+            echo $sheet->render($character);
+        }
 
         echo \PHP_EOL;
         exit($exitCode);
---
+++ src/Sheet.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace loyen\DndbCharacterSheet;
+
+use loyen\DndbCharacterSheet\Model\Character;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+
+class Sheet
+{
+    public function __construct(
+        private ?Environment $twig = null
+    )
+    {
+        $this->twig = $twig ?? new Environment(
+            new FilesystemLoader(dirname(__DIR__) . '/template')
+        );
+    }
+
+    public function render(
+        Character $character,
+        string $template = 'light-sheet.twig.html'
+    ) {
+        return $this->twig->load($template)->render([
+            'character' => $character
+        ]);
+    }
+}
